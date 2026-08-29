63aba46547f336756ad1dbaf863e56aa873de7df
lyx <965812158@qq.com>
2026-08-30 01:33:53 +0800

    add:补全侧边栏

--- jssh-front-end/src/App.vue
+++ jssh-front-end/src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <router-view />
+  <el-container style="height: 100vh">
+    <sidebarViews />
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
 </template>
+
+<script setup lang="ts">
+import sidebarViews from '@/views/sidebar/sidebarViews.vue'
+</script>
 
 <style>
 html,
@@ -10,5 +19,16 @@ body,
   padding: 0;
   width: 100%;
   height: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+
+#app .el-container {
+  height: 100%;
+}
+
+#app .el-main {
+  padding: 0;
+  overflow: hidden;
 }
 </style>
---
+++ jssh-front-end/src/views/sidebar/sidebarViews.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-aside width="220px">
+    <el-scrollbar>
+      <el-menu :default-active="route.path" router>
+        <el-menu-item index="/cmd">
+          <el-icon><Monitor /></el-icon>
+          <span>终端交互</span>
+        </el-menu-item>
+
+        <el-menu-item index="/memory">
+          <el-icon><Odometer /></el-icon>
+          <span>系统监控（内存/CPU/网络）</span>
+        </el-menu-item>
+
+        <el-menu-item index="/process">
+          <el-icon><Management /></el-icon>
+          <span>进程管理</span>
+        </el-menu-item>
+
+        <el-menu-item index="/files">
+          <el-icon><Folder /></el-icon>
+          <span>文件管理</span>
+        </el-menu-item>
+
+        <el-menu-item index="/network">
+          <el-icon><Connection /></el-icon>
+          <span>网络管理</span>
+        </el-menu-item>
+
+        <el-menu-item index="/services">
+          <el-icon><Setting /></el-icon>
+          <span>服务管理（systemctl）</span>
+        </el-menu-item>
+      </el-menu>
+    </el-scrollbar>
+  </el-aside>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { Monitor, Odometer, Management, Folder, Connection, Setting } from '@element-plus/icons-vue'
+
+const route = useRoute()
+</script>
